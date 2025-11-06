9910d2d8fd94926d751620a20e75d09332c3e957
tetli <182772561+tetli@users.noreply.github.com>
2025-11-06 14:25:45 +0100

    add email address

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -6,7 +6,9 @@ const Footer = () => {
         <footer className="site-footer">
             <div>
                 <h3>KTH Aero Design</h3>
-                {/* <p>Contact us at: ...</p> */}
+                <p>
+                    Contact us at: <a href="mailto:kthaerodesign@gmail.com" aria-label="Email kthaerodesign">kthaerodesign@gmail.com</a>
+                </p>
                 <p>Follow us on social media!</p>
                 <div className="footer-social-icons">
                     <a href="https://www.linkedin.com/company/kth-aero-design-team" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
