72526b1ef0387fbc079fa91d1e0ed9a8659847c2
tetli <182772561+tetli@users.noreply.github.com>
2026-02-06 11:02:24 +0100

    remove backend folder

--- backend/src/main/java/se/aerokth/backend/repository/ApplicationRepository.java
+++
@@ -1,6 +0,0 @@
-package se.aerokth.backend.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.aerokth.backend.model.Application;
-
-public interface ApplicationRepository extends JpaRepository<Application, Long> {}
-
